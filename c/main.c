e612f3277e00139c7f06deb1278a8bdec9a7eaaa
Paul Bowen-Huggett <paulhuggett@mac.com>
2021-10-18 16:17:02 +0200

    Simplify now that we have a "proper" C++ demo.
    Use a wildcard rule for the multi-target archive expansion.

--- c/main.c
+++ c/main.c
@@ -1,12 +1,5 @@
 #include <stdio.h>
 
-struct xtors {
-    xtors() { printf ("xtors::xtors\n"); }
-    ~xtors () { printf ("xtors::~xtors\n"); }
-};
-
-xtors x;
-
 int main (int argc, char * argv[], char * envp[]) {
     for (int ctr = 0 ; ctr < argc; ++ctr) {
         printf("%d: %s\n", ctr, argv[ctr]);
